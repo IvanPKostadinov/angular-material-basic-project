6c85c1867ba9ffce2c4c424b0b24cb59b5d0a252
IvanPKostadinov <ivanpkostadinov@gmail.com>
2021-10-27 10:29:23 +0300

    added routing

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from '../shared/material.module';
+import { FormsModule } from '@angular/forms';
+
 import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
 
@@ -11,7 +14,9 @@ import { ButtonsComponent } from './buttons/buttons.component';
   ],
   imports: [
     CommonModule,
-    DemoRoutingModule
+    MaterialModule,
+    FormsModule,
+    DemoRoutingModule,
   ]
 })
 export class DemoModule { }
