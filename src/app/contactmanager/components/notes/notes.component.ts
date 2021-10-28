5fdb654c69ef91ec422bc8e217f0ef5971f3d656
IvanPKostadinov <ivanpkostadinov@gmail.com>
2021-10-28 13:25:50 +0300

    added matTable with pagination and filtering

---
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -0,0 +1,38 @@
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Note } from '../../models/note';
+
+@Component({
+  selector: 'app-notes',
+  templateUrl: './notes.component.html',
+  styleUrls: ['./notes.component.scss']
+})
+export class NotesComponent implements OnInit, AfterViewInit {
+
+  @Input() notes!: Note[];
+
+  displayedColumns: string[] = ['position', 'title', 'date'];
+  dataSource!:  MatTableDataSource<Note>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
